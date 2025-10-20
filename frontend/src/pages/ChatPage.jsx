59c039e9e8f3be29d061431091e0cd47fe858cb9
Odusanya <odusanyasegun.tech@gmail.com>
2025-10-20 16:49:06 +0100

    Login and Logout UI Done

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useAuthStore } from "../store/useAuthStore"
 
 function ChatPage() {
-  
+  const {logout} = useAuthStore();
   return (
-    <div>
+    <div className='z-10'>
       <h1>hi chat</h1>
+      <button onClick={logout}>Log Out</button>
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,11 +3,11 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
 
-export const useAuthStore = create ((set) => ({
+export const useAuthStore = create((set) => ({
     authUser: null,
-    isCheckingAuth:true,
-    isSigningUp:false,
-    
+    isCheckingAuth: true,
+    isSigningUp: false,
+
     checkAuth: async () => {
         try {
             const res = await axiosInstance.get("/auth/check");
@@ -19,7 +19,7 @@ export const useAuthStore = create ((set) => ({
         }
     },
 
-    signup : async (data) => {
+    signup: async (data) => {
         set({ isSigningUp: true });
 
         try {
@@ -33,5 +33,32 @@ export const useAuthStore = create ((set) => ({
         } finally {
             set({ isSigningUp: false });
         }
-    }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+
+            toast.success("Logged in successfully");
+            //   get().connectSocket();
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+            // get().disconnectSocket();
+        } catch (error) {
+            toast.error("Error logging out");
+            console.log("Logout error:", error);
+        }
+    },
 }));
